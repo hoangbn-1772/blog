ced5df97beedc3d05b532adddcef7403bf8ec25f
hoangbn-1772 <48110404+hoangbn-1772@users.noreply.github.com>
2020-06-15 10:40:55 +0700

    task#3,4: Insert user into database and create screen list user (#5)
    Co-authored-by: bui.ngoc.hoang <SUN-ASTERISK\bui.ngoc.hoang@b121772-pc.sun-asterisk.com>

--- site/view/add.php
+++ site/view/add.php
@@ -1,3 +1,5 @@
+<?php require '../controller/UserController.php'; ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +15,7 @@
     <div class="content">
         <h3>Thêm người dùng</h3>
         <hr>
-        <form action="/site/controller/UserController.php" method="POST" enctype="multipart/form-data">
+        <form action="<?php store(); ?>" method="POST" enctype="multipart/form-data">
             <div class="flex-content">
                 <div class="label-item">
                     <label for="name">Họ và tên</label>
@@ -28,7 +30,7 @@
                     <input type="text" id="fullname" name="fullname" placeholder="Luka Modric" required>
                     <input type="text" id="email" name="email" placeholder="example@gmail.com" required>
                     <input type="date" id="birthday" name="birthday" required>
-                    <input type="tel" id="phone" name="phone" placeholder="0123-456-789" pattern="[0-9]{4}-[0-9]{2}-[0-9]{3}" required>
+                    <input type="tel" id="phone" name="phone" placeholder="0123-456-789" required>
                     <input type="text" id="address" name="address" required>
                     <div class="gender">
                         <input type="radio" id="male" name="gender" value="male" checked>
